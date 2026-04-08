a0b3e471a93f774f51ab7e570abd0fdaec7b5b9c
GabrielFP <gabrinandes10@gmail.com>
2026-04-08 17:06:42 -0500

    add packages to the os

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,10 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# this installs packages from fedora repos
+dnf5 install -y distrobox
+dnf5 install -y fastfetch
+dnf5 install -y btop
 
 # Use a COPR Example:
 #
